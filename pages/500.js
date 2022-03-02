b1a604ddca5e12e2e4696e327908be090b2db564
Mohd Masood Alam <130masoodalam@gmail.com>
2022-03-02 10:28:04 +0530

    Added 505 error page, and some styling to error pages

--- pages/500.js
+++ pages/500.js
@@ -4,8 +4,8 @@ import styles from '../styles/error.module.css'
 export default function Custom404() {
   return (
     <div className={styles.container}>
-      <div className={styles.emoji}>👽</div>
-      <h1 className={`${utilStyles.headingMd} ${styles.error}`}>404 <span className={styles.bar}>|</span> Not Found</h1>
+      <div className={styles.emoji}>🖥</div>
+      <h1 className={`${utilStyles.headingMd} ${styles.error}`}>500 <span className={styles.bar}>|</span> Internal Server Error</h1>
     </div>
   )
 }
